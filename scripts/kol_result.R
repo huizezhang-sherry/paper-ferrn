a0cc62d97b2e31b9e7558898da852e34993728cb
Sherry <huizezhangsh@gmail.com>
2021-01-21 18:26:48 +1100

    more work on noisy index simulation, WIP

--- scripts/kol_result.R
+++ scripts/kol_result.R
@@ -1,52 +1,25 @@
-## ----kol-better
-bind_boa6_theo <- function(dt){
-  dt %>%
-    bind_theoretical(matrix = matrix(c(0, 1, 0, 0, 0, 0), nrow = 6),
-                     index = tourr::norm_kol(nrow(boa6)),
-                     raw_data = boa6) %>%
-    bind_theoretical(matrix = matrix(c(0, 0, 1, 0, 0, 0), nrow = 6),
-                     index = tourr::norm_kol(nrow(boa6)),
-                     raw_data = boa6)
-}
-
-
+## ----kol-result
 clean %>%
-  filter(sim == "kol_better",method != "search_polish") %>%
-  bind_boa6_theo() %>%
-  explore_space_pca(group = seed, color = var_found,
+  filter(method != "search_polish") %>%
+  bind_theoretical(matrix = matrix(c(0, 1, 0, 0, 0, 0), nrow = 6),
+                   index = tourr::norm_kol(nrow(boa6)),
+                   raw_data = boa6) %>%
+  bind_theoretical(matrix = matrix(c(0, 0, 1, 0, 0, 0), nrow = 6),
+                   index = tourr::norm_kol(nrow(boa6)),
+                   raw_data = boa6) %>%
+  mutate(seed_sim = paste0(seed, sim),
+         alpha = ifelse(str_detect(sim, "tuned"), 0.7, 0.5),
+         search_name = ifelse(str_detect(sim, "random"), "search_better_random", "search_better")) %>%
+  compute_pca(group = seed_sim) %>%
+  pluck("aug") %>%
+  explore_space_pca(details = FALSE, pca = FALSE, group = seed_sim, color = var_found,
                     precision = 0.001, theo_size = 15,
-                    start_size = 7) +
+                    start_size = 1, end_size = 3,
+                    interp_size = 0.5) +
   scale_color_botanical() +
-  facet_wrap(vars(fct_reorder(seed, index_val_best)))
+  facet_grid(alpha ~ search_name, labeller = label_both)
 
-# kol_better_tuned
-kol_better_tuned <- clean %>%
-  filter(sim == "kol_better_tuned", method != "search_polish") %>%
-  bind_boa6_theo() %>%
-  explore_space_pca(group = seed, color = var_found,
-                    precision = 0.001, theo_size = 15,
-                    start_size = 7) +
-  scale_color_botanical() +
-  facet_wrap(vars(fct_reorder(seed, index_val_best)))
 
-## ----kol-random
-clean %>%
-  filter(sim == "kol_random",method != "search_polish") %>%
-  bind_boa6_theo() %>%
-  explore_space_pca(group = seed, color = var_found,
-                    precision = 0.001, theo_size = 15,
-                    start_size = 7) +
-  scale_color_botanical() +
-  facet_wrap(vars(fct_reorder(seed, index_val_best)))
 
-## ----kol-random-tuned
-clean %>%
-  filter(sim == "kol_random_tuned", method != "search_polish") %>%
-  bind_boa6_theo() %>%
-  explore_space_pca(group = seed, color = var_found,
-                    precision = 0.001, theo_size = 15,
-                    start_size = 7) +
-  scale_color_botanical() +
-  facet_wrap(vars(fct_reorder(seed, index_val_best)))
 
 
