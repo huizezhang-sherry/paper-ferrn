6be5b0580b23183c59a60b9bbc67c2e21300ab3b
Sherry <huizezhangsh@gmail.com>
2021-01-28 21:12:39 +1100

    text editing till PCA plot

--- scripts/toy.R
+++ scripts/toy.R
@@ -36,7 +36,7 @@ bind_rows(holes_1d_geo, holes_1d_better) %>%
   bind_theoretical(matrix(c(0, 1, 0, 0, 0), nrow = 5),
                    index = tourr::holes(), raw_data = boa5)  %>%
   explore_space_pca(group = method, details = TRUE,
-                    interp_size = 1, ratio = 7, dir_alpha = 0.5) +
+                    interp_size = 1, ratio = 5) +
   scale_color_discrete_botanical()
 
 ## ----toy-pca-animated
