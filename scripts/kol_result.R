10fc7c6b2288072d86bdc026350842846b9ec698
Sherry <huizezhangsh@gmail.com>
2021-02-01 12:01:41 +1100

    writing on the simulation example

--- scripts/kol_result.R
+++ scripts/kol_result.R
@@ -9,7 +9,7 @@ clean_pca <- clean %>%
                    raw_data = boa6) %>%
   mutate(seed_sim = paste0(seed, sim),
          alpha = ifelse(str_detect(sim, "tuned"), 0.7, 0.5),
-         search_name = ifelse(str_detect(sim, "random"), "search_better_random", "search_better"),
+         optimiser = ifelse(str_detect(sim, "random"), "SAJO", "SA"),
          var_found = as.factor(ifelse(var_found == "V7", "global", "local")),
          var_found = fct_relevel(var_found, "global", after = 1)) %>%
   compute_pca(group = seed_sim) %>%
@@ -18,19 +18,19 @@ clean_pca <- clean %>%
 global_max <- clean_pca %>%
   filter(info == "theoretical") %>%
   filter(index_val == max(index_val)) %>%
-  select(-alpha, -search_name)
+  select(-alpha, -optimiser)
 
 local_max <- clean_pca %>%
   filter(info == "theoretical") %>%
   filter(index_val == min(index_val)) %>%
-  select(-alpha, -search_name)
+  select(-alpha, -optimiser)
 
-clean_pca %>% drop_na() %>%
+clean_pca %>% drop_na() %>% rename(`search size` = alpha) %>%
   explore_space_pca(pca = FALSE, group = seed_sim, color = var_found,
-                    start_size = 1, end_size = 2,
+                    start_size = 1, start_alpha = 0.5, end_size = 2,
                     interp_size = 0.5) +
   add_theo(dt = global_max, theo_size = 20) +
   add_theo(dt = local_max, theo_label = "x", theo_size = 10) +
   scale_color_discrete_botanical() +
-  facet_grid(alpha ~ search_name, labeller = label_both)
+  facet_grid(`search size` ~ optimiser, labeller = label_both)
 
