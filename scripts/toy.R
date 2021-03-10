f06d502a6e831197bbb0e5ba32db1121da2b71f5
Sherry <huizezhangsh@gmail.com>
2021-03-10 22:22:59 +1100

    render graphs

--- scripts/toy.R
+++ scripts/toy.R
@@ -32,12 +32,12 @@ p2 <- holes_2d_better_random %>%
 
 p1 | p2
 
-## ---- toy-pca
+## ----toy-pca
 bind_rows(holes_1d_geo, holes_1d_better) %>%
   bind_theoretical(matrix(c(0, 1, 0, 0, 0), nrow = 5),
                    index = tourr::holes(), raw_data = boa5)  %>%
   explore_space_pca(group = method, details = TRUE,
-                    interp_size = 1, ratio = 9) +
+                    interp_size = 1) +
   scale_color_discrete_botanical()
 
 ## ----toy-pca-animated
